05d187992892b27de27de56547f484821d94c168
Niko Wilbert <mail@nikowilbert.de>
2026-05-25 13:36:20 +0200

    export pytest_given.Template from the package

--- src/pytest_given/__init__.py
+++ src/pytest_given/__init__.py
@@ -2,5 +2,14 @@
 
 from pytest_given.decorators import attach, given, scenario, then, when
 from pytest_given.errors import PytestGivenError
+from pytest_given.template import Template
 
-__all__ = ['PytestGivenError', 'attach', 'given', 'scenario', 'then', 'when']
+__all__ = [
+    'PytestGivenError',
+    'Template',
+    'attach',
+    'given',
+    'scenario',
+    'then',
+    'when',
+]
